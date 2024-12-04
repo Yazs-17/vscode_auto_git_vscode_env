4291324f9fbdb2011ce59d14f4419882300147ad
Sun Jiabin <152000985+Yazs-17@users.noreply.github.com>
2024-12-04 18:28:56 +0800

    Add files via upload
    complete the AutoGit Module

---
+++ vscode-alg-AutoGit/temp/1.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define fer(i, m, n) for (ll i = m; i < n; i++)
+using namespace std;
+void solve() {
+    
+}
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0), cout.tie(0);
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
