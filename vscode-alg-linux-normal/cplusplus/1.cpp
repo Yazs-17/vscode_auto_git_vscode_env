7fbac03b19b3e2ff0fb6bd2b3d6c1ff64d177ec0
Sun Jiabin <152000985+Yazs-17@users.noreply.github.com>
2025-02-16 01:40:26 +0800

    Add files via upload

---
+++ vscode-alg-linux-normal/cplusplus/1.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define fer(i, m, n) for (ll i = m; i < n; i++)
+using namespace std;
+void solve() {
+    int n;
+    cin >> n;
+    cout << n << endl;
+    cout << 44 << endl;
+}
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0), cout.tie(0);
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
